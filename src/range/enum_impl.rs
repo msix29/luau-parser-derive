4b9c5a4805db881d250a61d7e40944b78512a0c5
msix29 <abdulrehmanessam7567@gmail.com>
2025-03-31 00:18:25 +0300

    Add base for enums

--- src/range/enum_impl.rs
+++ src/range/enum_impl.rs
@@ -1,1 +1,7 @@
-pub fn r#impl() {}
+use proc_macro::TokenStream;
+use syn::{DataEnum, DeriveInput};
+
+#[inline]
+pub fn generate(input: &DeriveInput, data: &DataEnum) -> TokenStream {
+    todo!()
+}
--- src/range/mod.rs
+++ src/range/mod.rs
@@ -2,7 +2,6 @@ mod enum_impl;
 mod struct_impl;
 
 use proc_macro::TokenStream;
-use proc_macro2::Span;
 use quote::quote;
 use syn::{parse_macro_input, Data, DeriveInput};
 
@@ -11,7 +10,7 @@ pub fn generate(input: TokenStream) -> TokenStream {
 
     match input.data {
         Data::Struct(ref data_struct) => struct_impl::generate(&input, data_struct),
-        Data::Enum(data_enum) => todo!(),
+        Data::Enum(ref data_enum) => enum_impl::generate(&input, data_enum),
         Data::Union(_) => error!("`#[Derive(Range)]` can't be called on unions."),
     }
 }
