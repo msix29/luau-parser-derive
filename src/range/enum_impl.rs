e44641e57dac55d1a4a49980a938966d79b6248f
msix29 <abdulrehmanessam7567@gmail.com>
2025-03-31 13:07:21 +0300

    Support `range_or` attribute for enums
    Make sure no repeated fields exist (Invalid Rust error otherwise)

--- src/range/enum_impl.rs
+++ src/range/enum_impl.rs
@@ -2,6 +2,8 @@ use proc_macro2::{Ident, TokenStream};
 use quote::quote;
 use syn::{DataEnum, Fields, FieldsNamed, FieldsUnnamed};
 
+use super::utils::get_fallback;
+
 #[inline]
 pub fn generate(data: &DataEnum) -> TokenStream {
     let mut match_arms = Vec::new();
@@ -26,6 +28,20 @@ pub fn generate(data: &DataEnum) -> TokenStream {
     }
 }
 
+fn generate_for_fallback(name: &Ident, fallback: &Option<Ident>) -> TokenStream {
+    if fallback.is_some() {
+        quote! {{
+            if let Some(item) = #name {
+                item.get_range()
+            } else {
+                #fallback.get_range()
+            }
+        }}
+    } else {
+        quote! { #name.get_range() }
+    }
+}
+
 fn named(name: &Ident, fields: &FieldsNamed) -> TokenStream {
     let Some(first) = fields.named.first() else {
         must_have_one_item!();
@@ -33,10 +49,22 @@ fn named(name: &Ident, fields: &FieldsNamed) -> TokenStream {
     let Some(first_name) = first.ident.as_ref() else {
         must_have_one_item!();
     };
+    let (found, first_fallback) = get_fallback(first, first_name);
+    if found && first_fallback.is_none() {
+        return error!("`range_or` field must be a string literal.");
+    }
+
+    let first_body = generate_for_fallback(first_name, &first_fallback);
+
+    let mut referenced_fields = if let Some(first_fallback) = first_fallback {
+        vec![first_name.clone(), first_fallback]
+    } else {
+        vec![first_name.clone()]
+    };
 
     if fields.named.len() == 1 {
         quote! {
-            Self::#name { #first_name, .. } => #first_name.get_range(),
+            Self::#name { #(#referenced_fields,)* .. } => #first_body,
         }
     } else {
         let Some(last) = fields.named.last() else {
@@ -45,11 +73,25 @@ fn named(name: &Ident, fields: &FieldsNamed) -> TokenStream {
         let Some(last_name) = last.ident.as_ref() else {
             must_have_one_item!();
         };
+        let (found, last_fallback) = get_fallback(last, last_name);
+        if found && last_fallback.is_none() {
+            return error!("`range_or` field must be a string literal.");
+        }
+
+        let last_body = generate_for_fallback(last_name, &last_fallback);
+
+        referenced_fields.push(last_name.clone());
+
+        if Some(first_name) != last_fallback.as_ref() {
+            if let Some(last_fallback) = last_fallback {
+                referenced_fields.push(last_fallback);
+            }
+        }
 
         quote! {
-            Self::#name{ #first_name, #last_name, .. } => Ok(crate::types::Range::new(
-                #first_name.get_range()?.start,
-                #last_name.get_range()?.end,
+            Self::#name { #(#referenced_fields,)* .. } => Ok(crate::types::Range::new(
+                #first_body?.start,
+                #last_body?.end,
             )),
         }
     }
