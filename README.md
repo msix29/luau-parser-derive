30a4769cf6a6f15607b938b093aff569a194a351
msix29 <abdulrehmanessam7567@gmail.com>
2025-04-07 18:28:19 +0300

    Add `README.md`

---
+++ README.md
@@ -0,0 +1,3 @@
+# Luau Parser Derive
+
+This crate provides helpful derive procedural macros. It's only meant to be used by the [luau-parser](https://github.com/msix29/luau-parser/) crate.
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
-//! # Luau Parser Derive
-//!
-//! This crate provides helpful derive procedural macros. It's only meant to be used
-//! by the [luau-parser](https://github.com/msix29/luau-parser/) crate.
+#![doc = include_str!("../README.md")]
 
 #![deny(unsafe_code)]
 #![warn(clippy::missing_docs_in_private_items)]
